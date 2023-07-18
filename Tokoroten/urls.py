59dec3ba7bf16ee9f0eb8b647cae10672a310804
kimny1143 <kimny1143@gmail.com>
2023-07-19 00:10:41 +0900

    Update urls.py

--- Tokoroten/urls.py
+++ Tokoroten/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import delete_file
+from .views import delete_files
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('delete_file/<int:file_id>/', delete_file, name='delete_file'),
+    path('delete_file/<int:file_id>/', delete_files, name='delete_file'),
 
 ]
